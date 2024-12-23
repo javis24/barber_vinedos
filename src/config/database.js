7b9acc013d08ab0218abbab0eef29d1492734320
Javier Mtz <luisjaviermtzb@gmail.com>
2024-12-23 11:07:04 -0600

    update-horario

--- src/config/database.js
+++ src/config/database.js
@@ -7,11 +7,20 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD,
   {
     host: process.env.MYSQL_HOST,
-    dialect: process.env.MYSQL_DIALECT || 'mysql', 
-    dialectModule: require('mysql2'),
-    logging: false, 
-    timezone: "-06:00",
+    dialect: process.env.MYSQL_DIALECT || "mysql",
+    dialectModule: require("mysql2"),
+    logging: false,
+    timezone: "-06:00", // Configuración para la zona horaria
+    dialectOptions: {
+      timezone: "-06:00", // Compatibilidad adicional con MySQL
+      connectTimeout: 10000, // Opcional: Tiempo de espera para la conexión
+    },
   }
 );
+
+// Configurar la zona horaria al conectar
+sequelize.beforeConnect(async (config) => {
+  config.timezone = "-06:00"; // Configurar la zona horaria para la conexión
+});
 
 module.exports = sequelize;
